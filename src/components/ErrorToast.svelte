c15a4d9ff06705dec54761aa59abad497fdf22d1
DavidJonesADA <david.jones@ada.ac.uk>
2022-03-01 19:24:20 +0000

    Added error toast

---
+++ src/components/ErrorToast.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let error_message = "An error occured";
+</script>
+
+<div class="fixed top-0 right-0 m-2">
+  <div class="bg-red-500 text-white">
+    <hr />
+    <div class="p-4">
+      {error_message}
+    </div>
+  </div>
+</div>
--- src/pages/had_form/[id].svelte
+++ src/pages/had_form/[id].svelte
@@ -4,14 +4,18 @@
   import { onMount } from "svelte";
   import { had_form } from "./had_questions.js";
   import RadioInput from "../../components/RadioInput.svelte";
+  import ErrorToast from "../../components/ErrorToast.svelte";
 
   metatags.title = "HAD Form";
 
   const form_responses = {};
+  const { id } = $params;
+
+  let submitting = false;
+
+  let show_success = false;
 
   const validateID = (async () => {
-    const { id } = $params;
-
     const response = await fetch(
       `${import.meta.env.VITE_PROD}/validate_user/${id}`
     );
@@ -23,67 +27,120 @@
   })();
 
   const submit = () => {
-    console.log(form_responses);
+    submitting = true;
+
+    const total = Object.values(form_responses).reduce((acc, cur) => {
+      return acc + cur;
+    }, 0);
+
+    // make a post request to the server
+    fetch(`${import.meta.env.VITE_PROD}/submit_had_form/${id}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id: $params.id,
+        form_responses,
+        total,
+      }),
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          show_success = true;
+        } else {
+          throw new Error(response.statusText);
+        }
+      })
+      .catch(() => {
+        // create new error toast
+        new ErrorToast({
+          target: document.body,
+          props: {
+            error_message:
+              "There was an error submitting your form. Please contact your cardiac coach if this error persists.",
+          },
+        });
+      })
+      .finally(() => {
+        submitting = false;
+      });
   };
 </script>
 
-{#await validateID}
-  <div class="flex items-center justify-center">
-    <div
-      class="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full"
-      role="status"
-    >
-      <span class="visually-hidden">Loading...</span>
-    </div>
-  </div>
-{:then}
-  <div class="container mx-auto mt-1">
-    <h1 class="text-3xl font-bold">HAD Scale Form</h1>
-    <div class="bg-white-300 my-3">
-      <p>
-        Here at CP+R® we are aware that emotions and how you feel play an
-        important part in your overall health.
-      </p>
-
-      <p>
-        Knowing more about how you feel will ensure that we are able to help you
-        as effectively as possible. Read each item and tick the box that comes
-        closest to how you have been feeling in the past week.
-      </p>
-
-      <p>
-        Don’t take too long over your replies; your immediate reaction to each
-        item will probably be more accurate than a long thought-out response.
-      </p>
-    </div>
-
-    <form on:submit|preventDefault={submit}>
-      {#each had_form as question, x}
-        <div class="mb-2">
-          <h2 class="text-2xl font-bold">{question.title}</h2>
-          {#each question.options as option, y}
-            <RadioInput
-              name={question.title}
-              value={y}
-              bind:group={form_responses[`q_${x + 1}`]}
-              label={option}
-            />
-          {/each}
-        </div>
-      {/each}
-      <div class="flex">
-        <button
-          class="bg-yellow-300 py-1 px-2 mt-2 mb-7 mx-auto text-lg"
-          type="submit">Submit</button
-        >
+{#if show_success}
+  Success!
+{:else}
+  {#await validateID}
+    <div class="flex items-center justify-center">
+      <div
+        class="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full"
+        role="status"
+      >
+        <span class="visually-hidden">Loading...</span>
       </div>
-    </form>
-  </div>
-{:catch}
-  <div>
-    <p>
-      We were unable to find a form linked to this address. Please contact your
-      cardiac coach if this problem persists.
-    </p>
-  </div>
-{/await}
+    </div>
+  {:then}
+    <div class="container mx-auto mt-1">
+      <h1 class="text-3xl font-bold">HAD Scale Form</h1>
+      <div class="bg-white-300 my-3">
+        <p>
+          Here at CP+R® we are aware that emotions and how you feel play an
+          important part in your overall health.
+        </p>
+
+        <p>
+          Knowing more about how you feel will ensure that we are able to help
+          you as effectively as possible. Read each item and tick the box that
+          comes closest to how you have been feeling in the past week.
+        </p>
+
+        <p>
+          Don’t take too long over your replies; your immediate reaction to each
+          item will probably be more accurate than a long thought-out response.
+        </p>
+      </div>
+
+      <form on:submit|preventDefault={submit}>
+        {#each had_form as question, x}
+          <div class="mb-2">
+            <h2 class="text-2xl font-bold">{question.title}</h2>
+            {#each question.options as option, y}
+              <RadioInput
+                name={question.title}
+                value={y}
+                bind:group={form_responses[`q${x + 1}`]}
+                label={option}
+              />
+            {/each}
+          </div>
+        {/each}
+        <div class="flex">
+          <button
+            class="bg-yellow-300 py-1 px-2 mb-7 mx-auto text-lg flex items-center"
+            type="submit"
+          >
+            Submit
+            {#if submitting}
+              <div
+                class="ml-2 spinner-border animate-spin inline-block w-6 h-6 border-3 rounded-full text-white"
+                role="status"
+              >
+                <span class="visually-hidden">Loading...</span>
+              </div>
+            {/if}
+          </button>
+        </div>
+      </form>
+    </div>
+  {:catch}
+    <div>
+      <p>
+        We were unable to find a form linked to this address. Please contact
+        your cardiac coach if this problem persists.
+      </p>
+    </div>
+  {/await}
+{/if}
+
+<!-- create new ErrorToast on error -->
