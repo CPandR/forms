1c0b020699016e9ca5703ab496bf55eefcf1f417
DavidJonesADA <david.jones@ada.ac.uk>
2022-03-01 18:15:58 +0000

    Seperated radio input into seperate component

--- src/pages/had_form/[id].svelte
+++ src/pages/had_form/[id].svelte
@@ -2,109 +2,10 @@
   import { metatags } from "@roxi/routify";
   import { params } from "@roxi/routify";
   import { onMount } from "svelte";
+  import { had_form } from "./had_questions.js";
+  import RadioInput from "../../components/RadioInput.svelte";
 
   metatags.title = "HAD Form";
-
-  const form_data = [
-    {
-      title: "I feel tense or wound up",
-      options: [
-        "All of the time",
-        "A lot of the time",
-        "Occasionally",
-        "Not at all",
-      ],
-    },
-    {
-      title: "I still enjoy the things I used to enjoy",
-      options: [
-        "Definitely as much",
-        "Not quite so much",
-        "Only a little",
-        "Hardly at all",
-      ],
-    },
-    {
-      title:
-        "I get a sort of frightened feeling as if something awful is about to happen",
-      options: [
-        "Very definitely and quite badly",
-        "Yes, but not too badly",
-        "A little, but it doesn’t worry me",
-        "Not at all",
-      ],
-    },
-    {
-      title: "I can laugh and see the funny side of things",
-      options: [
-        "As much as I always could",
-        "Not quite so much now",
-        "Definitely not so much now",
-        "Not at all",
-      ],
-    },
-    {
-      title: "Worrying thoughts go through my mind",
-      options: [
-        "All of the time",
-        "A lot of the time",
-        "From time to time",
-        "Only occasionally",
-      ],
-    },
-    {
-      title: "I feel cheerful",
-      options: ["Not at all", "Not often", "Sometimes", "Most of the time"],
-    },
-    {
-      title: "I can sit at ease and feel relaxed",
-      options: ["Definitely", "Usually", "Not often", "Not at all"],
-    },
-    {
-      title: "I feel as if I am slowed down",
-      options: ["All the time", "Often", "Sometimes", "Not at all"],
-    },
-    {
-      title:
-        "I get a sort of frightened feeling like “butterflies” in the stomach",
-      options: ["Not at all", "Occasionally", "Quite often", "All the time"],
-    },
-    {
-      title: "I have lost interest and put less effort into my appearance",
-      options: [
-        "Definitely",
-        "Often",
-        "Sometimes",
-        "I take just as much care as ever",
-      ],
-    },
-    {
-      title: "I feel restless as if I have to be on the move",
-      options: [
-        "Very much indeed",
-        "Quite a lot",
-        "Not very much",
-        "Not at all",
-      ],
-    },
-    {
-      title: "I look forward with enjoyment to things",
-      options: [
-        "As much as I ever did",
-        "Rather less than I used to",
-        "Definitely less than I used to",
-        "Hardly at all",
-      ],
-    },
-    {
-      title: "I get sudden feelings of panic",
-      options: ["All the time", "Quite often", "Not very often", "Not at all"],
-    },
-    {
-      title: "I can relax and enjoy a good book or radio or TV programme",
-      options: ["Often", "Sometimes", "Not often", "Rarely"],
-    },
-  ];
 
   const form_responses = {};
 
@@ -123,7 +24,6 @@
 
   const submit = () => {
     console.log(form_responses);
-    console.log("I'm the validate() function");
   };
 </script>
 
@@ -158,11 +58,11 @@
     </div>
 
     <form on:submit|preventDefault={submit}>
-      {#each form_data as question, x}
+      {#each had_form as question, x}
         <div class="mb-2">
           <h2 class="text-2xl font-bold">{question.title}</h2>
           {#each question.options as option, y}
-            <div>
+            <!-- <div>
               <label>
                 <input
                   class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
@@ -170,14 +70,23 @@
                   name={`${question.title}`}
                   value={y}
                   bind:group={form_responses[`question_${x}`]}
+                  required
                 />
                 {option}
               </label>
+            </div> -->
+            <div>
+              <RadioInput
+                name={question.title}
+                value={y}
+                bind:group={form_responses[`question_${x}`]}
+                label={option}
+              />
             </div>
           {/each}
         </div>
       {/each}
-      <button type="submit">Submit</button>
+      <button class="bg-yellow-300 py-1 px-2 my-2" type="submit">Submit</button>
     </form>
   </div>
 {:catch}
---
+++ src/pages/had_form/had_questions.js
@@ -0,0 +1,95 @@
+export const had_form = [
+  {
+    title: "I feel tense or wound up",
+    options: [
+      "All of the time",
+      "A lot of the time",
+      "Occasionally",
+      "Not at all",
+    ],
+  },
+  {
+    title: "I still enjoy the things I used to enjoy",
+    options: [
+      "Definitely as much",
+      "Not quite so much",
+      "Only a little",
+      "Hardly at all",
+    ],
+  },
+  {
+    title:
+      "I get a sort of frightened feeling as if something awful is about to happen",
+    options: [
+      "Very definitely and quite badly",
+      "Yes, but not too badly",
+      "A little, but it doesn’t worry me",
+      "Not at all",
+    ],
+  },
+  {
+    title: "I can laugh and see the funny side of things",
+    options: [
+      "As much as I always could",
+      "Not quite so much now",
+      "Definitely not so much now",
+      "Not at all",
+    ],
+  },
+  {
+    title: "Worrying thoughts go through my mind",
+    options: [
+      "All of the time",
+      "A lot of the time",
+      "From time to time",
+      "Only occasionally",
+    ],
+  },
+  {
+    title: "I feel cheerful",
+    options: ["Not at all", "Not often", "Sometimes", "Most of the time"],
+  },
+  {
+    title: "I can sit at ease and feel relaxed",
+    options: ["Definitely", "Usually", "Not often", "Not at all"],
+  },
+  {
+    title: "I feel as if I am slowed down",
+    options: ["All the time", "Often", "Sometimes", "Not at all"],
+  },
+  {
+    title:
+      "I get a sort of frightened feeling like “butterflies” in the stomach",
+    options: ["Not at all", "Occasionally", "Quite often", "All the time"],
+  },
+  {
+    title: "I have lost interest and put less effort into my appearance",
+    options: [
+      "Definitely",
+      "Often",
+      "Sometimes",
+      "I take just as much care as ever",
+    ],
+  },
+  {
+    title: "I feel restless as if I have to be on the move",
+    options: ["Very much indeed", "Quite a lot", "Not very much", "Not at all"],
+  },
+  {
+    title: "I look forward with enjoyment to things",
+    options: [
+      "As much as I ever did",
+      "Rather less than I used to",
+      "Definitely less than I used to",
+      "Hardly at all",
+    ],
+  },
+  {
+    title: "I get sudden feelings of panic",
+    options: ["All the time", "Quite often", "Not very often", "Not at all"],
+  },
+  {
+    title: "I can relax and enjoy a good book or radio or TV programme",
+    options: ["Often", "Sometimes", "Not often", "Rarely"],
+  },
+];
